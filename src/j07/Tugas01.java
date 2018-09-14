d776ac252e7c049417f767d492f08c7c53bb9b0c
WINDOWS 10 <WINDOWS 10@DESKTOP-EO5EC5V>
2018-09-14 20:41:39 +0700

    initial commit

---
+++ src/j07/Tugas01.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j07;
+import java.util.Scanner;
+public class Tugas01 {
+    public static void main(String[] args) {
+        int [] nominal = new int [] {5000, 2000, 1000, 500, 100};
+        int uang, i, jumlah, n = nominal.length;
+        
+        String Identitas  = "Adelia Fitri Kristanti/ X RPL 2 / 02";
+        System.out.println("Identitas: " + Identitas);
+        System.out.println("");
+       
+        
+        }
+    }
+
