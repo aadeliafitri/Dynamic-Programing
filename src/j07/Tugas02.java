162bbac3f9e42fccc4e333f4c99f9860970f0807
WINDOWS 10 <WINDOWS 10@DESKTOP-EO5EC5V>
2018-09-14 21:10:48 +0700

    Initial Commit

---
+++ src/j07/Tugas02.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j07;
+import java.util.Scanner;
+public class Tugas02 {
+    public static void main(String[] args) {
+        int [] kilogram = new int [] {7, 5, 4, 3, 2};
+        int nominal[]= {10000, 7000, 5000, 3000, 2000};
+        char [] jenis = new char [] {'E','B','A','D','C'};
+        
+        String Identitas  = "Adelia Fitri Kristanti/ X RPL 2 / 02";
+        System.out.println("Identitas: " + Identitas);
+        System.out.println("");
+        
+       
+    }
+}
+       
+    
+
